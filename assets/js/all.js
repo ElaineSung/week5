43a0a40ed38fe039738d042b88b1d689f0ed17f7
elaine <elaine7062002@gmail.com>
2021-08-09 22:31:45 +0800

    Update 2021-08-09T14:31:43.046Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,12 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+$('#js-open-editor').click(function (e) {
+  e.preventDefault();
+  $('.editor').addClass('active');
+});
+$('#js-close-editor').click(function (e) {
+  e.preventDefault();
+  $('.editor').removeClass('active');
+});
 //# sourceMappingURL=all.js.map
